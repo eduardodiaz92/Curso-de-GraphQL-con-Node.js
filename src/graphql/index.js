3cdb0d636781b9995025e8e9cf7afe51ff0fcd75
Eduardo Díaz <eduaedodiaz@gmail.com>
2023-06-13 16:39:02 -0600

    archivos.graphql

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -6,19 +6,35 @@ const {
 } = require('@apollo/server/plugin/landingPage/default');
 // Middleware de Express también en @apollo/server
 const { expressMiddleware } = require('@apollo/server/express4');
-const typeDefs = `
-  type Query {
-    hello: String
-  }
-`;
+
+const { loadFiles } = require('@graphql-tools/load-files');
+
 const resolvers = {
   Query: {
     hello: () => 'Hola mundillo',
+    getPerson: (_, args) =>
+      `Hello, my name is ${args.name}, I'm ${args.age} yold`,
+    getInt: (_, args) => args.age,
+    getFloat: (_, args) => args.price,
+    getString: () => 'qwerty',
+    getBoolean: () => true,
+    getID: () => '123456',
+    getNumers: (_, args) => args.numbers,
+    getProduct: () => {
+      return {
+        id: '123',
+        name: 'product 1',
+        price: 123,
+        description: 'tool',
+        image: 'qweergdrg.com',
+        createdAt: new Date().toISOString(),
+      };
+    },
   },
 };
 const useGraphQL = async (app) => {
   const server = new ApolloServer({
-    typeDefs,
+    typeDefs: await loadFiles('./src/**/*.graphql'),
     resolvers,
     playground: true,
     plugins: [ApolloServerPluginLandingPageLocalDefault],
